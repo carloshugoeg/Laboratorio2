ac232366b9224ca47ca950402ccd27403277874f
Carlos Hugo Escobar Gomez <chescobarg@correo.url.edu.gt>
2024-08-07 15:38:20 -0600

    Se hace avance corto en el manu principal

--- Laboratorio2/Program.cs
+++ Laboratorio2/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Laboratorio2;
+using System.Runtime.InteropServices;
 
 List<Habitacion> Hotel = new List<Habitacion>();
+Habitacion habitacion = new Habitacion(0, 0);
 do
 {
     Console.Clear();
@@ -16,7 +18,7 @@ do
     switch (opcion)
     {
         case "1":
-            AddDevice(electronicDevices);
+            AgregarHabitacion(Hotel);
             break;
         case "2":
             foreach (var device in electronicDevices)
@@ -39,3 +41,74 @@ do
             break;
     }
 } while (true);
+void AgregarHabitacion(List<Habitacion> hotel)
+{
+    Console.WriteLine("-----AGREGAR HABITACION------");
+    Console.WriteLine("\n     1. Habitacion Simple");
+    Console.WriteLine("\n     2. Habitacion Doble");
+    Console.WriteLine("\n     3. Suite");
+    Console.WriteLine("\n     4. Habitacion Deluxe");
+    string opcion = Console.ReadLine();
+    Console.Write("\nNumero Habitacion: ");
+    string name = Console.ReadLine();
+    double precio = habitacion.PedirPrecio();
+    switch (opcion)
+    {
+        case "1":
+            Console.Write("Numero de Camas: ");
+            int numCamas = PedirNumero();
+
+
+            break;
+        case "2":
+            Console.WriteLine("");
+            
+        case "3":
+          
+    }
+}
+
+int PedirNumero()
+{
+    do
+    {
+        try
+        {
+
+            int numero;
+            do
+            {
+                numero = int.Parse(Console.ReadLine());
+                if (numero == 0 || numero == null)
+                {
+                    Console.WriteLine("Valor no puede ser 0");
+                    Console.ReadLine();
+                }
+            } while (numero == 0);
+            return numero;
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("INPUT INVALIDO");
+            Console.WriteLine(ex.Message);
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("-----Nueva Habitacion-----");
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine("EL numero es demasiado grande");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("-----Nueva Habitacion-----");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("ERROR");
+            Console.WriteLine(ex.Message);
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("-----Nueva Habitacion-----");
+        }
+    } while (true);
+}
